dad05335128dbb460fc3e00f2bafb922be7668fb
Dev-Azairn <116623679+dev-azairn@users.noreply.github.com>
2026-02-16 19:24:57 +0700

    backbone

--- OS Bank System Simulation/src/BankTester.java
+++ OS Bank System Simulation/src/BankTester.java
@@ -1,8 +1,9 @@
-// File:
-// Author:
+
+// Author: 6788094 Piyapong Pongruang
 // Description:
 
 public class BankTester {
+    
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
---
+++ OS Bank System Simulation/src/Transaction.java
@@ -0,0 +1,12 @@
+public class Transaction {
+    private BankAccount from;
+    private BankAccount to;
+
+    public Transaction(BankAccount from, BankAccount to)
+    {
+        this.from = from;
+        this.to = to;
+    }
+
+    
+}
