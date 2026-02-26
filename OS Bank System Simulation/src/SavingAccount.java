46d0906d8110e85e170de1140fb52eb7120a0ffe
Khomgrit Kongjaroen <145886271+SakMin1234@users.noreply.github.com>
2026-02-26 18:01:39 +0700

    Update SavingAccount.java

--- OS Bank System Simulation/src/SavingAccount.java
+++ OS Bank System Simulation/src/SavingAccount.java
@@ -1,30 +1,41 @@
-// File:
-// Author:
-// Description:
+// File: SavinngAccount
+// Author: Khomgrit Kongjaroen
+// Description: For user that wants to save money
 
 public class SavingAccount extends BankAccount {
 
-    
+    private static double interestRate = 0.02; // 2% default
 
     public SavingAccount(User accoutOwner, String accountNo, double initBalance, String accountType) {
         super(accoutOwner, accountNo, initBalance, "SavingAccount");
     }
-
     
     public SavingAccount(String citizenId, String name, String telephoneNo, String address, User accoutOwner,
             String accountNo, double initBalance, String accountType) {
         super(citizenId, name, telephoneNo, address, accoutOwner, accountNo, initBalance, "SavingAccount");
     }
 
+    // Getter & Setter for interest rate
+    public static void setInterestRate(double rate) {
+        if (rate > 0) {
+            interestRate = rate;
+        }
+    }
+
+    public static double getInterestRate() {
+        return interestRate;
+    }
+
     // Implement Validation according to account type
     @Override
     public boolean validate() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return getBalance() >= 500
     }
 
     @Override
     public void earnInterest() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        double interest = getBalance() * interestRate;
+        deposit(interest);
     }
 
 }
