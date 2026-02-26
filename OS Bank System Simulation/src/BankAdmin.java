f56be3aa22447e998d5abe9d08ec71835c342bc8
6788064 <nuttanun.mun@student.mahidol.edu>
2026-02-26 22:40:19 +0700

    Update BankAdmin.java

--- OS Bank System Simulation/src/BankAdmin.java
+++ OS Bank System Simulation/src/BankAdmin.java
@@ -1,33 +1,66 @@
-// File:
-// Author:
-// Description:
+// File:BankAdmin
+// Author:Nuttanun Muanraksa
+// Description:Checking password and using with users class
 import java.util.Arrays;
 import java.util.List;
 
 
-// Implement BankAdmin
 public class BankAdmin {
+
     public enum AccessLevel {ROOT, EDIT, VIEW};
-    public static List<BankAdmin> adminList = Arrays.asList(new BankAdmin("admin", "1234", AccessLevel.ROOT));
+
+    public static List<BankAdmin> adminList =
+        Arrays.asList(new BankAdmin("admin", "1234", AccessLevel.ROOT));
 
     private String username;
     private String password;
     private AccessLevel accessLevel;
 
-    // Task 1: Implement constructor, username need to be unique
     public BankAdmin(String username, String password, AccessLevel accessLevel)
     {
-        
+        if (adminList != null) {
+            for (BankAdmin admin : adminList) {
+                if (admin.username.equals(username)) {
+                    throw new IllegalArgumentException("Username already exists");
+                }
+            }
+        }
+
+        this.username = username;
+        this.password = password;
+        this.accessLevel = accessLevel;
     }
 
-    // Task 2: add setter, can be set by admin with edit and root level only
+    public void setPassword(String newPassword, BankAdmin requester) {
+        if (requester.accessLevel == AccessLevel.ROOT ||
+            requester.accessLevel == AccessLevel.EDIT) {
+            this.password = newPassword;
+        } else {
+            throw new SecurityException("Access denied");
+        }
+    }
 
+    public void setAccessLevel(AccessLevel newLevel, BankAdmin requester) {
+        if (requester.accessLevel == AccessLevel.ROOT ||
+            requester.accessLevel == AccessLevel.EDIT) {
+            this.accessLevel = newLevel;
+        } else {
+            throw new SecurityException("Access denied");
+        }
+    }
 
-    // Task 3: use to validate for update interest
-    // Hint: Check all username, password need to match.
     public static boolean validateAdmin(String username, String password)
     {
+        for (BankAdmin admin : adminList) {
+            if (admin.username.equals(username) &&
+                admin.password.equals(password)) {
+                return true;
+            }
+        }
         return false;
     }
 
+    public AccessLevel getAccessLevel() {
+        return accessLevel;
+    }
 }
